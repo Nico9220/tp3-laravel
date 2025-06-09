96036d98878c2780169c0ccef0a04041bb0c28b8
abialmiron <abialmiron@gmail.com>
2025-06-09 15:53:31 -0300

    cambio en nombre

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>MyBlog Laravel</title>
+    <title> Comunidad Laravel </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Tailwind CDN -->
